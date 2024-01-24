cb87c046572f9d8158321cff585f73a4cd4cb7ab
chksaikumar <chksaikumar.0297@gmail.com>
2024-01-24 17:29:57 -0600

    python code 1st class

---
+++ cal.py
@@ -0,0 +1,16 @@
+first_Number = 5
+second_number = 2
+
+
+print ("Addition :" , first_Number + second_number)
+print ("Subtraction :", first_Number - second_number)
+print("Multiplication :" , first_Number * second_number)
+print ("Division : " ,first_Number / second_number)
+print ("Integer division :", first_Number // second_number)
+print("Modulo :", first_Number % second_number)
+print("Exponentiation :" , first_Number ** second_number)
+
+
+print ("Area of circle : ",(3.1415)*(5**2))
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hello")
+
+
+print (6163 )
+
+# printing course details by using default print function
+print ("SAI KUMAR " + "CSCI 6163" \
+       + "Python program Language and Lab")
+
+
+
+MY_NAME = "sai kumar"
+MY_sNumber = 439777
+print (MY_NAME,MY_sNumber)
