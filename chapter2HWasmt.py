5e909345bb882050f1212d8369bf6698628e9532
CHKSAIKUMAR <68428805+chksaikumar@users.noreply.github.com>
2024-01-25 19:30:13 -0600

    Create chapter2HWasmt.py
    chapter 2 hw asmt

---
+++ chapter2HWasmt.py
@@ -0,0 +1,11 @@
+print("The Area and Perimeter program")
+print()
+length = int(input("Please enter the length: "))
+width = int(input("Please enter the width: "))
+print()
+area = length * width
+perimeter = 2 * (length + width)
+print("Area = " + str(area))
+print("Perimeter = " + str(perimeter))
+print()
+print("Bye!")
