3ee8aee59b27856cbdde9617d48234668a4498f1
CHKSAIKUMAR <68428805+chksaikumar@users.noreply.github.com>
2024-01-25 19:03:47 -0600

    Create chapter1cw.py
    Create chapter1cw

---
+++ chapter1cw.py
@@ -0,0 +1,19 @@
+print("welcome to the Future Value Calculator")
+print()
+
+choice = "y"
+while choice.lower() == "y":
+  monthly_investment = float(input("Enter monthly investment:\t"))
+  yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
+  years = int(input("Enter number of years:\t\t"))
+  monthly_interest_rate = yearly_interest_rate / 12 / 100
+  months = years * 12
+  future_value = 0
+  for i in range(months):
+    future_value += monthly_investment
+    monthly_interest_amount = future_value * monthly_interest_rate
+    future_value += monthly_interest_amount
+  print("Future value:\t\t\t" + str(round(future_value, 2)))
+  print()
+  choice = input("Continue? (y/n): ")
+print("Bye!")
